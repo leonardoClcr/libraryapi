f63ebf16eb255eacc5bcf88f7eda32ac61968a6f
Leonardo C Rocha <rochalcr.dev@gmail.com>
2025-10-23 11:45:26 -0300

    adicionado a entidade Autor

---
+++ src/main/java/io/github/leonardoClcr/libraryapi/model/Autor.java
@@ -0,0 +1,29 @@
+package io.github.leonardoClcr.libraryapi.model;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDate;
+import java.util.UUID;
+
+@Entity
+@Table(name="autor")
+@Getter
+@Setter
+public class Autor {
+
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(name = "name", length = 100, nullable = false)
+    private String nome;
+
+    @Column(name = "data_nascimento", nullable = false)
+    private LocalDate dataNascimento;
+
+    @Column(name = "nacionalidade", nullable = false, length = 50)
+    private String nacionalidade;
+}
