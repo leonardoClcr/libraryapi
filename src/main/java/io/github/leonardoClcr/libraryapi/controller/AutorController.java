9db39f09128f7ef017ba29ca11e07782a7e8c3dd
Leonardo C Rocha <rochalcr.dev@gmail.com>
2025-10-30 21:03:36 -0300

    Finalizado Aula 79

---
+++ src/main/java/io/github/leonardoClcr/libraryapi/controller/AutorController.java
@@ -0,0 +1,10 @@
+package io.github.leonardoClcr.libraryapi.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("autores")
+public class AutorController {
+
+}
